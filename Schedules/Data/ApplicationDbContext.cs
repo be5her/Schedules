bd07663178c2aee760af2d90520f94c2c5a667ab
be5her <besher.karam.1998@hotmail.com>
2021-06-06 16:05:39 +0300

    adding the classes to the project

--- Schedules/Data/ApplicationDbContext.cs
+++ Schedules/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Schedules.Models;
+using Schedules_classes;
 
 namespace Schedules.Data
 {
@@ -14,5 +15,28 @@ namespace Schedules.Data
         {
 
         }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Channel> Channels { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Order_Services> Order_Services { get; set; }
+
+        public DbSet<Order_Workers> Order_Workers { get; set; }
+
+        public DbSet<Payment> Payments { get; set; }
+
+        public DbSet<School> Schools { get; set; }
+
+        public DbSet<Semester> Semesters { get; set; }
+
+        public DbSet<Service> Services { get; set; }
+
+        public DbSet<Stage> Stages { get; set; }
+
+        public DbSet<Worker> Workers { get; set; }
+
     }
 }
