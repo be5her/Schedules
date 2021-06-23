bf95ad85900eae1fd794a6807dfaad6648bcdf31
be5her <besher.karam.1998@hotmail.com>
2021-06-23 17:48:07 +0300

    changging the signin manger IdentityUser to ApplicationUser

--- Schedules/Models/ApplicationUser.cs
+++ Schedules/Models/ApplicationUser.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace Schedules.Models
 {
+    [NotMapped]
     public class ApplicationUser : IdentityUser
     {
+        public ApplicationUser()
+        {
+
+        }
+
         public string First_name { get; set; }
 
         public string Last_name { get; set; }
 
+        public string Name()
+        {
+            return First_name + " " + Last_name;
+        }
     }
 }
--- Schedules/Startup.cs
+++ Schedules/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Schedules.Data;
+using Schedules.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,8 +31,10 @@ namespace Schedules
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
