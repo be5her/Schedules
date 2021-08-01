57b9aa023325f0d3268050c6f3c1c871eecfe89d
be5her <besher.karam.1998@hotmail.com>
2021-08-02 01:42:57 +0300

    adding service model

---
+++ Logic/Model/ServiceModel.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Schedules_classes;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Model
+{
+    public class ServiceModel
+    {
+        public static async Task<List<Service>> GetAllAsync()
+        {
+            using (var _context = new DB())
+            {
+                var applicationDbContext = _context.Services.Include(s => s.AspNetUser);
+                return await applicationDbContext.ToListAsync();
+            }
+        }
+
+        public static async Task<Service> GetServiceAsync(int? id)
+        {
+            using (var _context = new DB())
+            {
+                var service = await _context.Services
+                .Include(c => c.AspNetUser)
+                .FirstOrDefaultAsync(m => m.Service_id == id);
+                return service;
+            }
+        }
+
+        public static async Task<bool> CreateAsync(Service service)
+        {
+            using (var _context = new DB())
+            {
+                _context.Services.Add(service);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public static async Task<bool> UpdateAsync(Service service)
+        {
+            using (var _context = new DB())
+            {
+                var _service = await _context.Services.SingleOrDefaultAsync(e => e.Service_id == service.Service_id);
+                if (_service == null)
+                {
+                    return false;
+                }
+                _service.Name = service.Name;
+                _service.Price = service.Price;
+                await _context.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public static async Task<bool> DeleteAsync(int id)
+        {
+            using (var _context = new DB())
+            {
+                var service = await _context.Services.SingleOrDefaultAsync(e => e.Service_id == id);
+                if (service == null)
+                {
+                    return false;
+                }
+                var order_service = await _context.Order_services.FirstOrDefaultAsync(e => e.Service_id == id);
+                if (order_service != null)
+                {
+                    return false;
+                }
+                _context.Services.Remove(service);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public static List<SelectListItem> GetSelectList(int? selected = null)
+        {
+            using (var _context = new DB())
+            {
+                List<SelectListItem> list;
+                if (selected == null)
+                {
+                    list = new SelectList(_context.Channels, "Channel_id", "Name").ToList();
+                }
+                else
+                {
+                    list = new SelectList(_context.Channels, "Channel_id", "Name", selected).ToList();
+                }
+                return list;
+            }
+        }
+
+    }
+}
---
+++ Schedules/Controllers/ServicesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Logic.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Schedules.Data;
+using Schedules_classes;
+
+namespace View.Controllers
+{
+    public class ServicesController : Controller
+    {
+
+        // GET: Services
+        public async Task<IActionResult> Index()
+        {
+            return View(await ServiceModel.GetAllAsync());
+        }
+
+        // GET: Services/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await ServiceModel.GetServiceAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // GET: Services/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Service_id,Name,Price")] Service service)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            service.Added_by = userId;
+            service.Added_date = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                await ServiceModel.CreateAsync(service);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Services/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await ServiceModel.GetServiceAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Service_id,Name,Price")] Service service)
+        {
+            if (id != service.Service_id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await ServiceModel.UpdateAsync(service);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Services/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await ServiceModel.GetServiceAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // POST: Services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await ServiceModel.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
